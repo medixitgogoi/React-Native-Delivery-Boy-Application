b0db6d9650cd1158c230e32a32dce0a02de84102
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-10-29 12:55:58 +0530

    updated

---
+++ src/screens/DeliveredOrders.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const DeliveredOrders = () => {
+  return (
+    <View>
+      <Text>DeliveredOrders</Text>
+    </View>
+  )
+}
+
+export default DeliveredOrders;
